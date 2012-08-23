a994d052872c031edb7a00d336431ef62fb14e9e
Toshiro Yamada <toshiroyamada@users.noreply.github.com>
2012-08-23 09:28:16 -0700

    specifically pull from origin master

--- vim/update_bundles.zsh
+++ vim/update_bundles.zsh
@@ -2,10 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for f in `find bundle -type d -name .git`; do
+  echo 
   echo $f
   gitdir=${f%.git}
   cd $gitdir 
-  git pull
+  git pull origin master
   cd $DIR
 done
 
