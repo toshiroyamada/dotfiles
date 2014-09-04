e467dc72938478d0a401395059d6db5647c4424c
toshiroyamada <toshiroyamada@users.noreply.github.com>
2014-09-04 12:26:11 -0700

    Add script to setup dotfiles

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+
+relpath=$(python -c "import os.path; print os.path.relpath('${DIR}', '${HOME}')")
+
+echo "Link files"
+for conf in vim vimrc zsh zshrc tmux.conf; do
+    echo ln -s "${relpath}/$conf" "${HOME}/.$conf"
+    ln -s "${relpath}/$conf" "${HOME}/.$conf"
+done
+
+echo "Create swap, backup and undo folders"
+mkdir -p ~/.vimlocal/{backup,swap,undo}
+
+echo Install vundle
+mkdir vim/bundle
+git clone https://github.com/gmarik/vundle.git vim/bundle/vundle
+
+echo Install vim bundles
+vim +BundleInstall +qall
+
+echo Ready. Now compile YouCompleteMe: http://valloric.github.com/YouCompleteMe/
+
+# On Linux, we need to install the following dependencies
+# sudo apt-get install build-essential git cmake python-dev
