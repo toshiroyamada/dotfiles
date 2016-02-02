fbab00771b64f32fc89ead9d772ea9dcd652d335
Toshiro Yamada <toshiroyamada@users.noreply.github.com>
2016-02-02 09:56:54 -0800

    Update tmux installer to 2.1

--- installs/install-tmux.sh
+++ installs/install-tmux.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
+
+LIBEVENT_VERSION=2.0.22-stable
+TMUX_VERSION=2.1
+
 if [ $(uname -s) = "Darwin" ]; then
     brew install tmux
 else
@@ -12,22 +17,32 @@ else
 
     # Install libevent
     cd ${SRCDIR}
-    wget https://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-2.0.22-stable.tar.gz
-    tar xzf libevent-2.0.22-stable.tar.gz
-    cd libevent-2.0.22-stable
-    ./configure --prefix=${OPTDIR} --disable-shared
-    make
-    make install
+    LIBEVENT_DIR=libevent-${LIBEVENT_VERSION}
+    if [ ! -d "${LIBEVENT_DIR}" ]; then
+        wget https://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-${LIBEVENT_VERSION}.tar.gz
+        tar xzf libevent-${LIBEVENT_VERSION}.tar.gz
+        cd "${LIBEVENT_DIR}"
+        ./configure --prefix=${OPTDIR} --disable-shared
+        make
+        make install
+    else
+        echo "${LIBEVENT_DIR} already exists!"
+    fi
 
     # Install tmux
     cd ${SRCDIR}
-    wget http://downloads.sourceforge.net/tmux/tmux-1.9a.tar.gz
-    tar xzf tmux-1.9a.tar.gz
-    cd tmux-1.9a
+    TMUX_DIR=tmux-${TMUX_VERSION}
+    if [ ! -d "${TMUX_DIR}" ]; then
+        wget https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+        tar xzf tmux-${TMUX_VERSION}.tar.gz
+        cd "${TMUX_DIR}"
 
-    ./configure --enable-static  --prefix=${OPTDIR} \
-        CFLAGS="-I${OPTDIR}/include" LDFLAGS="-L${OPTDIR}/lib" \
-        LIBEVENT_CFLAGS="-I${OPTDIR}/incude" LIBEVENT_LIBS="-L${OPTDIR}/lib -levent"
-    make -j5
-    make install
+        ./configure --enable-static  --prefix=${OPTDIR} \
+            CFLAGS="-I${OPTDIR}/include" LDFLAGS="-L${OPTDIR}/lib" \
+            LIBEVENT_CFLAGS="-I${OPTDIR}/incude" LIBEVENT_LIBS="-L${OPTDIR}/lib -levent"
+        make -j5
+        make install
+    else
+        echo "${TMUX_DIR} already exists!"
+    fi
 fi
